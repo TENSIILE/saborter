9a42164415d04bb4238e454054f1dd302b4dfc71
TENSIILE <laptev-vlad2001@yandex.ru>
2025-12-29 01:56:44 +0300

    fix: настраивает vite

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import cleanPlugin from 'vite-plugin-clean';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      rollupTypes: true,
+      bundledPackages: [],
+    }),
+    cleanPlugin(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Saborter',
+      formats: ['es', 'cjs'],
+      fileName: format => {
+        if (format === 'es') return 'index.es.js';
+        if (format === 'cjs') return 'index.cjs.js';
+        return `index.${format}.js`;
+      },
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        preserveModules: false,
+        globals: {},
+      },
+    },
+    sourcemap: true,
+    minify: false,
+    target: 'es2020',
+  },
+});
