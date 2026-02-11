6c32fed72a60d565a342386b2cf906c2d1419777
TENSIILE <laptev-vlad2001@yandex.ru>
2026-02-12 00:16:52 +0300

    refactor: adds jsdoc

--- src/features/event-listener/event-listener.lib.ts
+++ src/features/event-listener/event-listener.lib.ts
@@ -1,6 +1,9 @@
 import { clearMethodSymbol } from './event-listener.constants';
 import { EventListener } from './event-listener';
 
+/**
+ * Сalls a private instance method to clear all listeners and data.
+ */
 export const clearEventListeners = (instance: EventListener) => {
   instance[clearMethodSymbol]();
 };
--- src/features/state-observer/state-observer.lib.ts
+++ src/features/state-observer/state-observer.lib.ts
@@ -2,10 +2,16 @@ import * as Constants from './state-observer.constants';
 import { StateObserver } from './state-observer';
 import { RequestState } from './state-observer.types';
 
-export const emitRequestState = (instance: StateObserver, requestState: RequestState) => {
+/**
+ * Calls a private method on the instance to notify listeners that a state change has occurred.
+ */
+export const emitRequestState = (instance: StateObserver, requestState: RequestState): void => {
   instance[Constants.emitMethodSymbol](requestState);
 };
 
-export const clearStateListeners = (instance: StateObserver) => {
+/**
+ * Calls a private instance method to clear all listeners and data.
+ */
+export const clearStateListeners = (instance: StateObserver): void => {
   instance[Constants.clearMethodSymbol]();
 };
