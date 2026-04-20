ab4b1f1133a9a7fc78e0cb350c852d18df97b545
TENSIILE <laptev-vlad2001@yandex.ru>
2026-04-20 23:22:59 +0300

    feat(dev): adds setAborterContextProvisionToFetchMode (#79)

--- src/dev.ts
+++ src/dev.ts
@@ -1,2 +1,3 @@
 export { setLoggerMode } from './shared/logger';
 export { setDebugErrorStackMode } from './features/extended-stack-error';
+export { setAborterContextProvisionToFetchMode } from './features/lib/fetch';
