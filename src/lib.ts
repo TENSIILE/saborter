9adbc0237df3944b21cb60488105b0238d36d10d
TENSIILE <laptev-vlad2001@yandex.ru>
2026-04-21 19:53:44 +0300

    feat(isTimeoutError): adds function to determine whether the error is a timeout error (#80)

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,4 @@
 export * from './features/lib';
 export * from './features/abort-error/abort-error.lib';
 export * from './modules/aborter/aborter.lib';
+export * from './features/timeout/timeout-error.lib';
